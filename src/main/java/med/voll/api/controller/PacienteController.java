b1668ed2d81e4bf141f6fc04937751cec36eed25
dpc-profile <daniel.pcastro11@gmail.com>
2023-04-14 17:55:20 -0300

    Adding persistence on database

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,6 +1,5 @@
 package med.voll.api.controller;
 
-
 import med.voll.api.paciente.DadosCadastroPaciente;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
